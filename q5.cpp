d87a0a385ad6e1c8e6983de7b91d8d04adef2ce9
Muhmmad Shaban <97331866+muhmmadshaban@users.noreply.github.com>
2024-10-28 11:49:02 +0530

    Q5 WITH EXPLAINATION

--- q5.cpp
+++ q5.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 /*Write a C++ function that checks if an array is a palindrome (i.e., it reads the same forwards and backwards).*/
 void chechPalindrome(int arr[],int size){
-    int st=0;
-    int end=size-1;
-    bool isPalin=false;
+    int st=0; //INTIALISE START WITH ZERO
+    int end=size-1;// INITIALIZE END WITH SIZE - 1 ,TO ACCESS LAST ELEMENT OF THE ARR
+    bool isPalin=false; // THIS IS A BOOLEAN VARIABLE, WE USE IT WHILE CHECKING THE ARRAY IF THE LAST ELEMENT OF THE ARRAY IS EQAUL TO THE FRIST ELEMENT OF THE ARRAY, I
+    // UPDATE THS isPalin TO THE TRUE AND UPDATE START WITH STRAT + 1 AND DECREMENT END WITH -1, BECAUSE WE NEED TO TRAVEL LEST TO RIGHT FROM THE START AND RIGHT TO LEFT FROM THE END, 
+    //IF NOT UPDATE isPalin TO THE FALSE, IF ARRAY [ST] NOT EQUAL TO THE ARRAAY[END], JUST BREAK THE LOOP, BECAUSE IF ANY VALUE PAIR IS NOT EQUAL IN THE WHOLE ARRAY, SO THERE IS NO MEAN TO CHECK ANY OTHER INDEX VALUE,
+    //IN THE EACH ITERATION OF THE LOOP
+    /*
+    AFTER WHEN ALL ITERATION ARE OVER, NOW CHECK IS isPalin IS EQUAL TO TRUE, IF YES, PRINT "PALINDROME ", IF NOT PRINT "NOT PALINDROME "
+    */
     while (st<=end)
     {
         if (arr[st]==arr[end])
